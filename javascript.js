7a90d801ab447825587ff1143468ddf5d9f634d1
levani-hacker <114279610+levani-hacker@users.noreply.github.com>
2023-07-23 16:56:32 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var years = 13;
+Name = "Levani";
+username = "tsakadze";
+zedmetsaxeli = "chikeksa";
+studing = "Goal_oriented academy";
+
+z = console.log("Hello, my name and username is " + Name + " " + username +", i am " + years + " years old. my zedmetsaxeli is " + zedmetsaxeli + ". i am studying in " + studing + "." );
